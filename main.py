5cc75b2e7619fab37614258cd45be11b383fb355
Alexander Melnikov <Msan90546@mail.ru>
2023-04-12 22:26:15 +0300

    add first version of game

--- main.py
+++ main.py
@@ -6,13 +6,50 @@ from flask_login import LoginManager, login_user, logout_user, login_required, c
 from forms.login import LoginForm
 from flask import make_response
 from flask_restful import Api
-
+from stockfish import Stockfish
+import chess
+import chess.svg
+from forms.input_move_form import MoveForm
 
 app = Flask(__name__)
 api = Api(app)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 login_manager = LoginManager()
 login_manager.init_app(app)
+board = chess.Board()
+my_stockfish = Stockfish('stockfish_15.1_win_x64_popcnt'
+                         '/stockfish-windows-2022-x86-64-modern.exe')
+
+
+def is_correct_move(move, board):
+    for x in list(board.legal_moves):
+        if move == str(x):
+            return True
+    return False
+
+
+@app.route('/start_game', methods=['GET', 'POST'])
+def display_field():
+    form = MoveForm()
+    board_svg = chess.svg.board(board=board)
+    field_file = open('static/img/photo_board.svg', "w")
+    field_file.write(board_svg)
+    while not board.is_checkmate() or not board.is_variant_draw():
+        if form.validate_on_submit():
+            fl = is_correct_move(form.move.data, board)
+            if fl:
+                get_move = chess.Move.from_uci(form.move.data)
+                board.push(get_move)
+                board_svg = chess.svg.board(board=board)
+                field_file = open('static/img/photo_board.svg', "w")
+                field_file.write(board_svg)
+                my_stockfish.set_fen_position(board.fen())
+                best_move = chess.Move.from_uci(my_stockfish.get_best_move())
+                board.push(best_move)
+                board_svg = chess.svg.board(board=board)
+                field_file = open('static/img/photo_board.svg', "w")
+                field_file.write(board_svg)
+        return render_template('display_field.html', title='Игра', form=form)
 
 
 def main():
