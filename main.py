2158d00960b5922faadb459ccba1dd4547107c5e
Nastya Stepanova <Nastya.stepanova1104@yandex.ru>
2023-04-18 21:09:02 +0300

    improvements

--- main.py
+++ main.py
@@ -29,12 +29,35 @@ def is_correct_move(move, board):
     return False
 
 
+def get_rating():
+    try:
+        id = current_user.id
+        db_sess = db_session.create_session()
+        rating = db_sess.query(Rating).filter(Rating.user_id == id).first()
+        user = db_sess.query(User).filter(User.id == id).first()
+        data_reg = user.modified_date
+        today = datetime.datetime.today()
+        d = (today - data_reg)
+        if d.days:
+            hours = d.days * 24 + int((str(d).split(', ')[1]).split(':')[0])
+        else:
+            hours = int((str(d).split(':')[0]))
+    except AttributeError:
+        rating = None
+        hours = None
+    return rating, hours
+
+
 @app.route('/start_game/<int:level>', methods=['GET', 'POST'])
 def display_field(level):
+    print(level)
+    if not current_user.is_authenticated:
+        return redirect("/")
     form = MoveForm()
     board_svg = chess.svg.board(board=board)
     field_file = open('static/img/photo_board.svg', "w")
     field_file.write(board_svg)
+    rating, hours = get_rating()
     while not board.is_checkmate() or not board.is_variant_draw():
         if form.validate_on_submit():
             fl = is_correct_move(form.move.data, board)
@@ -50,7 +73,8 @@ def display_field(level):
                 board_svg = chess.svg.board(board=board)
                 field_file = open('static/img/photo_board.svg', "w")
                 field_file.write(board_svg)
-        return render_template('display_field.html', title='Игра', form=form)
+        return render_template('display_field.html', title='Игра', form=form, rating=rating,
+                               hours=hours)
 
 
 def main():
@@ -81,21 +105,7 @@ def load_user(user_id):
 
 @app.route("/")
 def index():
-    try:
-        id = current_user.id
-        db_sess = db_session.create_session()
-        rating = db_sess.query(Rating).filter(Rating.user_id == id).first()
-        user = db_sess.query(User).filter(User.id == id).first()
-        data_reg = user.modified_date
-        today = datetime.datetime.today()
-        d = (today - data_reg)
-        if d.days:
-            hours = d.days * 24 + int((str(d).split(', ')[1]).split(':')[0])
-        else:
-            hours = int((str(d).split(':')[0]))
-    except AttributeError:
-        rating = None
-        hours = None
+    rating, hours = get_rating()
     return render_template("index.html", rating=rating, title='Главная страница', hours=hours)
 
 
