f09d3aca5778d2c9f741abff237bf68cb3372f45
Nastya Stepanova <Nastya.stepanova1104@yandex.ru>
2023-04-21 21:07:50 +0300

    rework the database

--- data/users.py
+++ data/users.py
@@ -17,7 +17,6 @@ class User(SqlAlchemyBase, UserMixin, SerializerMixin):
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     rating = orm.relationship("Rating", back_populates='user')
-    games = orm.relationship("Games", back_populates='user')
 
     def set_password(self, password):
         self.hashed_password = generate_password_hash(password)
